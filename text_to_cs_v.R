a5b7f21e0bccac5c808759595d37bd7d1f627bae
snailwellington <34104706+snailwellington@users.noreply.github.com>
2018-08-27 22:21:13 +0300

    Update text_to_cs_v.R

--- text_to_cs_v.R
+++ text_to_cs_v.R
@@ -1,12 +1,9 @@
-# library(xlsx)
-# setwd("//energia.sise/dfs/REDIRECT/tarmo.rahmonen/Documents/Pingekvaliteet/P?ringud")
+
 temp = file.choose()
-# 
-# for (i in 1:length(temp)){
-  temp2= gsub(".txt","",temp)
+temp2= gsub(".txt","",temp)
   tab = read.delim2(temp, sep=",",fileEncoding="UTF-16LE")
   tab = tab[,-ncol(tab)]
-  # setwd("C:/Users/tarmo.rahmonen/ISIKLIK")
+
   
   names(tab) <- c("start","end",
                  "I1_min","I1_max",
@@ -17,11 +14,6 @@ temp = file.choose()
                  "U31_min","U31_max")
   
   write.csv2(tab, paste0(temp2,".csv"), row.names = FALSE)
-  
-  # write.xlsx(tab, paste0(temp2[i],"_1",".xlsx"),sheetName = "Data")
-
-# }
-
 
 
 
